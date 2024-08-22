f1918878b8b5598d5aa5fa7c29699037825d9123
advadva <113452747+LuisOscar09@users.noreply.github.com>
2024-08-22 21:11:54 +0300

    Add files via upload

---
+++ src/Constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+  PREFIX: '!', 
+  BALANCE_PRICE: 1000, ////سعر الكوينز
+  BALANCE_LOG: '981576596488089600',///روم تم شراء
+  STOCK_CHANNEL: '1005105816011018310', ////روم كمية
+  STOCK_MESSAGE: '1276212442103283775',///رسالة الكمية
+  CLIENTS_ROLE: '1271229564227420192', //////رتبة شراء عادية
+  DONE_CHANNEL: '981576596488089600', //////روم تم شراء 
+  SUPERCLIENTS_ROLE: '1271229564227420192', ////رتبة شراء فخمة
+  MIN_MEMBERS: 1,
+  RECIPIENT_ID: '918477422242967594',////البنك
+  TRANSACTIONS_CHANNEL: '981576596488089600', ////روم التحويل 
+  CLIENT_ID: '978588170566398003', ////id تبع بوت
+  AUTH_URL: 'https://discord.com/oauth2/authorize?client_id=978588170566398003&response_type=code&redirect_uri=https%3A%2F%2F7b97be74-82e3-486f-9cf3-b41e89d40a3a-00-1dsd3x33dr5dh.spock.replit.dev%2F&scope=guilds.join+identify',///هنا تحط رابط ادخال الاعضاء
+  BOT_URL: 'https://discord.com/api/oauth2/authorize?client_id=1181040789632073778&permissions=8&scope=bot',
+  PROBOT_IDS: ['282859044593598464', '567703512763334685'], 
+  OWNERS: ['626071995796291603']////الاونر
+};
